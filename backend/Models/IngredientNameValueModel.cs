577a36c443767559d4a5a1877c4e8643ff539459
TheNewUserForCourse <111561496+TheNewUserForCourse@users.noreply.github.com>
2022-08-29 17:07:52 +0200

    Add files via upload

---
+++ backend/Models/IngredientNameValueModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace backend
+{
+    public class IngredientNameValueModel
+    {
+        public string zutat_name { get; set; }
+        public short verringern_um { get; set; }
+
+        public IngredientNameValueModel()
+        {
+
+        }
+    }
+}
